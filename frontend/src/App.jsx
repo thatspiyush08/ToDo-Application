55692983ed450e4ef91f427eb122f300d2261fd8
thatspiyush08 <piyushnaula008@gmail.com>
2024-01-25 18:34:06 +0530

    TODO App works fine now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,13 @@ import { CreateTodo } from './components/CreateTodo'
 import { Todos } from './components/Todos'
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
+
+  fetch("http://localhost:3000/todo")
+  .then(async (res)=>{
+    const json=await res.json();
+    setTodos(json.todos);
+  })
 
   return (
     <>
--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,15 +1,16 @@
-
+import { useState } from "react";
 
 
 
 export function CreateTodo(){
+
     return <div className="newTodo">
-        <input type="text" style={{
+        <input id="title" type="text" style={{
             margin:10,
             padding:10,
             borderRadius:5
         }} placeholder="Title.." />
-        <input type="text" style={{
+        <input type="text" id="desc" style={{
             margin:10,
             padding:10,
             paddingRight:90 ,
@@ -18,6 +19,20 @@ export function CreateTodo(){
         <button className="card" style={{
             margin:10,
             padding:10
+        }} onClick={()=>{
+            fetch("http://localhost:3000/todo",{
+                method:"POST",
+                body:JSON.stringify({
+                    title:document.getElementById("title").innerHTML,
+                    description:document.getElementById("desc").innerHTML
+                }),
+                headers:{
+                    "Content-type":"application/json"
+                }
+            }).then(async (res)=>{
+                const json= await res.json();
+                alert("TODO Added");
+            })
         }}>ADD TODO</button>
     </div>
 }
