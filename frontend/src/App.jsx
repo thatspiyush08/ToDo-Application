013ff914ead66a8271934b3b41b3ff4f5da86af0
thatspiyush08 <piyushnaula008@gmail.com>
2024-02-29 01:30:06 +0530

    added the add descripiton and add title

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,103 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { CreateTodo } from './components/CreateTodo'
-import { Todos } from './components/Todos'
+import React, { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import Todo from './assets/todo.png';
+import './App.css';
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todo, settodo] = useState([
+    {
+      title: 'Go to GYM',
+      description: 'From 12am-1am',
+    },
+    {
+      title: 'Do Front-End',
+      description: 'From 1am-3am',
+    },
+    {
+      title: 'Sleep',
+      description: 'From 3am-11am',
+    },
+  ]);
 
-  fetch("http://localhost:3000/todo")
-  .then(async (res)=>{
-    const json=await res.json();
-    setTodos(json.todos);
-  })
+  const [newTodo, setNewTodo] = useState({
+    title: '',
+    description: '',
+  });
+
+  function addTodo() {
+    if (newTodo.title && newTodo.description) {
+      settodo((prevTodo) => [...prevTodo, newTodo]);
+      setNewTodo({
+        title: '',
+        description: '',
+      });
+    }
+  }
+
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setNewTodo((prevTodo) => ({
+      ...prevTodo,
+      [name]: value,
+    }));
+  }
 
   return (
     <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={Todo} className="todo logo" alt="todo logo" />
+        </a>
+      </div>
+      <h1>To Do Application</h1>
+      <div className="card">
+        <div>
+          <input
+            type="text"
+            name="title"
+            value={newTodo.title}
+            onChange={handleInputChange}
+            placeholder="Enter Todo Name"
+            className="input-box"
+          />
+          <input
+            type="text"
+            name="description"
+            value={newTodo.description}
+            onChange={handleInputChange}
+            placeholder="Enter Todo Description"
+            className="input-box"
+          />
+          <button onClick={addTodo} className="add-button">Add Todo</button>
+        </div>
+        {todo.map(function (todos, index) {
+          return (
+            <TODOS
+              key={index}
+              title={todos.title}
+              description={todos.description}
+            ></TODOS>
+          );
+        })}
+      </div>
+
       <p className="read-the-docs">
-        Click on the Button to add TODO.
+        Click on the button to Add Random TODO's.
       </p>
-      <h1>TODO-APPLICATION</h1>
-
-
-      <CreateTodo></CreateTodo>   
-      <Todos todos={todos}></Todos>  
     </>
-  )
+  );
 }
 
-export default App
+function TODOS(props) {
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </div>
+  );
+}
+
+export default App;
